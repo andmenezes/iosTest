0034ef93cb70bc5777a37213ab05bde748c30163
Andre Menezes <ios.andrem@gmail.com>
2023-08-22 07:06:26 -0300

    Initial commit of iOS Carrefour Challenge

---
+++ iOS Carrefour Challenge/iOS Carrefour Challenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS Carrefour Challenge
+//
+//  Created by André Menezes on 22/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS Carrefour Challenge/iOS Carrefour Challenge/iOS_Carrefour_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Carrefour_ChallengeApp.swift
+//  iOS Carrefour Challenge
+//
+//  Created by André Menezes on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Carrefour_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
